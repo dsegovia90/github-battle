5aecfadc96849aae932a6ea1fe954f82d55d4054
Daniel Segovia <daniel@dabici.mx>
2017-06-27 10:19:49 -0500

    Added the router

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,29 @@
 var React = require('react')
 var Popular = require('./Popular')
+var ReactRouter = require('react-router-dom')
+var Router = ReactRouter.BrowserRouter
+var Route = ReactRouter.Route
+var Nav = require('./Nav')
+var Home = require('./Home')
+var Battle = require('./Battle')
+var Switch = ReactRouter.Switch
 
 class App extends React.Component {
   render() {
     return (
+      <Router>
       <div className='container'>
-        <Popular />
+        <Nav />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/battle' component={Battle} />
+          <Route path='/popular' component={Popular} />
+          <Route render={function(){
+            return <p>404 Not Found!</p>
+          }} />
+        </Switch>
       </div>
+      </Router>
     )
   }
 }
