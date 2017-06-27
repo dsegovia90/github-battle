ccef1b9e8db9188925b875582b5058a22f66f98c
Daniel Segovia <daniel@dabici.mx>
2017-06-27 10:20:58 -0500

    added the params for github api

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,9 @@
 var axios = require('axios')
 
+var id = 'cliendId'
+var sec = 'secretId'
+var params = '?client_id=' + id + '&client_secret=' + sec
+
 module.exports = {
   fetchPopularRepos: function (language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories')
